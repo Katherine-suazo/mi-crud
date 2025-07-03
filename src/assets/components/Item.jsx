d2eb7b7ce8078c9979aca707e7fe5ea875633209
Katherine <katherinesuazo032@gmail.com>
2025-07-02 20:30:57 -0400

    mejorar la estructura del componente Item y agregar estilos a la clase papa

--- src/assets/components/Item.jsx
+++ src/assets/components/Item.jsx
@@ -3,21 +3,22 @@ import React from 'react';
 function Item({ item, deleteItem, editItem }) {
   return (
 
-    <div>
+    <>
+      <div className='papa'>
+        <div className='textLeft'>
+          <p><strong>Alumno: {item.nombre} </strong></p>
+          <p>Asignatura: {item.asignatura}</p>
+          <p>Promedio: <strong>{parseFloat(item.promedio).toFixed(1)}</strong> </p>
+          {item.promedio == 7 && <span className='destacado'>Destacado</span>}
+        </div>
 
-      <div className='textLeft'>
-        <p><strong>Alumno: {item.nombre} </strong></p>
-        <p>Asignatura: {item.asignatura}</p>
-        <p>Promedio: <strong>{parseFloat(item.promedio).toFixed(1)}</strong> </p>
-        {item.promedio == 7 && <span className='destacado'>Destacado</span>}
+        <div className='botones'>
+          <button className="btn btn-outline-warning botones" onClick={() => editItem(item)}>Editar</button>
+          <button className="btn btn-outline-danger botones"  onClick={() => deleteItem(item.id)}>Eliminar</button>
+        </div>
       </div>
+    </>
 
-      <div className='botones'>
-        <button className="btn btn-outline-warning botones" onClick={() => editItem(item)}>Editar</button>
-        <button className="btn btn-outline-danger botones"  onClick={() => deleteItem(item.id)}>Eliminar</button>
-      </div>
-
-    </div>
   );
 }
 
